5c72dfc9e20854dad27d3c18831c4d3396544400
WebStory <webstory@live.jp>
2025-09-09 20:45:14 +0900

    小说页面中只显示签约作家

--- dark/static/javascript/authorinfo.js
+++ dark/static/javascript/authorinfo.js
@@ -1,1 +1,18 @@
-var authorInfoV3={"LogoImg":null,"Notes":null,"UId":0}
+function getByPenName(authorName, callback) {
+    var data = { penName: authorName};
+    $.ajax({
+        url: "/author/getByPenName",
+        type: "get",
+        dataType: 'json',
+        async: false,
+        data: data,
+        cache: false,
+        success: function (result) {
+            if (result.data) {
+                callback(result.data);
+            } else {
+                console.log("[E]Failed to get author data.", result);
+            }
+        }
+    });;
+}
